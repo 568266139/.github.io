21afc11c32f787b0680409561cf637224a5663a4
568266139 <52781764+568266139@users.noreply.github.com>
2019-10-12 16:10:53 +0800

    Add files via upload

---
+++ suninyigou2/home.js
@@ -0,0 +1,18 @@
+// var menuli=document.getElementsByClassName("menu-ul-li");
+// var daohang=document.getElementsByClassName("munu-daohang");
+// for(let i=0;i<menuli.length;i++){
+//     // console.log(menuli[i]);
+//     menuli[i].addEventListener("mouseover",function(){
+//         // console.log(menuli[i].style);
+//         menuli[i].style.border="1px rgb(171, 172, 173) solid";
+//         menuli[i].style.borderBottom="none";
+
+//         // console.log(menuli[i].style.border);
+//     })
+
+//     menuli[i].addEventListener("mouseout",function(){
+//         // console.log(menuli[i].style);
+//         menuli[i].style.border="none";
+ 
+//     })
+// }
